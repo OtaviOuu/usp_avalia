7c0cb3fa046260148fc63ce96b4044b867db4966
otaviouu <otavio.venancio.oliveira@usp.br>
2025-12-01 21:58:01 -0300

    auth generator

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :usp_avalia, :scopes,
+  user: [
+    default: true,
+    module: UspAvalia.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: UspAvalia.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :usp_avalia,
   ecto_repos: [UspAvalia.Repo],
   generators: [timestamp_type: :utc_datetime]
