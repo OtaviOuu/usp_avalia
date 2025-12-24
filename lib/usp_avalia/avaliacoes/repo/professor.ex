29095343eb1dafb9bb2e694f92eba90c0e94b1b7
otaviouu <otavio.venancio.oliveira@usp.br>
2025-12-24 01:29:40 -0300

    Refactor Professor repository: optimize queries by merging select statements and improve discipline count retrieval

--- lib/usp_avalia/avaliacoes/repo/professor.ex
+++ lib/usp_avalia/avaliacoes/repo/professor.ex
@@ -14,25 +14,39 @@ defmodule UspAvalia.Avaliacoes.Repo.Professor do
       from p in Professor,
         join: d in assoc(p, :disciplinas),
         where: d.codigo == ^codigo,
-        preload: [:disciplinas]
+        preload: [:disciplinas],
+        select_merge: %{
+          id: p.id,
+          nome: p.nome,
+          email: p.email,
+          salario: p.salario
+        }
 
     query
-    |> Repo.all()
     |> feed_numero_disciplinas()
+    |> Repo.all()
   end
 
   def get_all do
     Professor
     |> preload(:disciplinas)
-    |> Repo.all()
     |> feed_numero_disciplinas()
+    |> Repo.all()
   end
 
   # fazer na db
-  defp feed_numero_disciplinas(professores) do
-    Enum.map(professores, fn professor ->
-      numero_disciplinas = length(professor.disciplinas || [])
-      Map.put(professor, :numero_disciplinas, numero_disciplinas)
-    end)
+  defp feed_numero_disciplinas(query) do
+    from p in query,
+      select_merge: %{
+        numero_disciplinas:
+          fragment(
+            "(
+            SELECT COUNT(*)
+            FROM professores_disciplinas AS pd2
+            WHERE pd2.professor_id = ?
+          )",
+            p.id
+          )
+      }
   end
 end
